d67ab4857414650a379e68923a74cb340f7ef9d9
PIYUSH-FUNDE <149001998+piyushFunde@users.noreply.github.com>
2026-01-03 21:29:45 +0530

    Update DBhelper.java

--- DBhelper.java
+++ DBhelper.java
@@ -3,11 +3,16 @@ package OOPS.Project;
 import java.sql.*;
 
 public class DBhelper {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/your_database_name";
-    static final String USER = "root";
-    static final String PASS = "";
+    private static final String URL = "jdbc:mysql://localhost:3306/payroll_system";
+    private static final String USER = "root";
+    private static final String PASS = "";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, USER, PASS);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("JDBC Driver not found!");
+        }
     }
 }
